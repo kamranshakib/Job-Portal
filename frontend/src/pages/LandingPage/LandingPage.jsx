afc7b5ea16fdcd8f6d4efd21af9e64ac0fed671d
khaledsaifee <saifeekhaled1212@gmail.com>
2026-02-02 22:56:21 +0430

    Add files via upload

---
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Components/Header'
+
+function LandingPage() {
+  return (
+    <div className='min-h-screen'>
+      <Header/>
+    </div>
+  )
+}
+
+export default LandingPage
