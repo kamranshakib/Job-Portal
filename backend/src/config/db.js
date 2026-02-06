2341d4903a8e43da1fe62bc915d12a26b7587532
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-06 20:11:20 +0430

    add local mongo url in env file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/27017/job-portal");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database Connected");
   } catch (error) {
     console.error("Error Connecting to Database");
