927c095516b2e521e55bfe361937e41bdb016f7e
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-09 13:46:18 +0430

    update-profile

--- backend/src/routes/auth.Routes.js
+++ backend/src/routes/auth.Routes.js
@@ -3,9 +3,9 @@ import { register, login, getMe } from "../controllers/auth.Controller.js";
 import { protect } from "../middlewares/auth.Middleware.js";
 import upload from "../middlewares/upload.Middleware.js";
 
-const router = express.Router();
+const router = express.Router();  
 
-router.post("/register", register);
+router.post("/register", register); 
 router.post("/login", protect, login);
 router.post("/me", protect, getMe);
 
