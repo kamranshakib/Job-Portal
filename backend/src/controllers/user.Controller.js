7deb4eb96e925157af6fcea673f96a7c4b36ae4d
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-09 20:36:50 +0430

    get public profile

--- backend/src/controllers/user.Controller.js
+++ backend/src/controllers/user.Controller.js
@@ -76,6 +76,10 @@ export const deleteResume = async (req, res) => {
 // Get user public profile
 export const getPublicProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
